d15a5e35f59aaf6fe859bd008ea52e48692685ab
Artyom Belikin <biloplus@yandex.ru>
2026-05-26 16:57:14 +0300

    locators and interface added

--- tests/main-page.test.ts
+++ tests/main-page.test.ts
@@ -1,51 +1,106 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
 
-const elements = [
+interface Elements {
+  locator: (page: Page) => Locator;
+  name: string;
+  text?: string;
+  attribute?: {
+    type: string;
+    value: string;
+  };
+}
+
+const elements: Elements[] = [
   {
-    locator: (page) => page.getByRole('link', { name: 'Playwright logo Playwright' }),
+    locator: (page: Page): Locator =>
+      page.getByRole('link', { name: 'Playwright logo Playwright' }),
     name: 'Playwright logo',
     text: 'Playwright',
+    attribute: {
+      type: 'href',
+      value: '/',
+    },
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'Docs' }),
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'Docs' }),
     name: 'Docs link',
     text: 'Docs',
+    attribute: {
+      type: 'href',
+      value: '/docs/intro',
+    },
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'MCP', exact: true }),
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'MCP', exact: true }),
     name: 'MCP link',
     text: 'MCP',
+    attribute: {
+      type: 'href',
+      value: '/mcp/introduction',
+    },
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'CLI', exact: true }),
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'CLI', exact: true }),
     name: 'CLI link',
     text: 'CLI',
+    attribute: {
+      type: 'href',
+      value: '/agent-cli/introduction',
+    },
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'API', exact: true }),
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'API', exact: true }),
     name: 'API link',
     text: 'API',
+    attribute: {
+      type: 'href',
+      value: '/docs/api/class-playwright',
+    },
   },
   {
-    locator: (page) => page.getByRole('button', { name: 'Node.js' }),
+    locator: (page: Page): Locator => page.getByRole('button', { name: 'Node.js' }),
     name: 'Node.js link',
     text: 'Node.js',
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'GitHub repository' }),
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'GitHub repository' }),
     name: 'Github icon link',
+    attribute: {
+      type: 'href',
+      value: 'https://github.com/microsoft/playwright',
+    },
   },
   {
-    locator: (page) => page.getByRole('link', { name: 'Discord server' }),
-    name: 'Driscord icon link',
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'Discord server' }),
+    name: 'Discord icon link',
+    attribute: {
+      type: 'href',
+      value: 'https://aka.ms/playwright/discord',
+    },
   },
   {
-    locator: (page) => page.getByRole('button', { name: 'Switch between dark and light' }),
+    locator: (page: Page): Locator =>
+      page.getByRole('button', { name: 'Switch between dark and light' }),
     name: 'Dark mode switch',
   },
   {
-    locator: (page) => page.getByRole('button', { name: 'Search (Ctrl+K)' }),
+    locator: (page: Page): Locator => page.getByRole('button', { name: 'Search (Ctrl+K)' }),
     name: 'Search input',
+  },
+  {
+    locator: (page: Page): Locator =>
+      page.getByRole('heading', { name: 'Playwright enables reliable' }),
+    name: 'Title',
+    text: 'Playwright enables reliable',
+  },
+  {
+    locator: (page: Page): Locator => page.getByRole('link', { name: 'Get started' }),
+    name: 'Get started link',
+    text: 'Get started',
+    attribute: {
+      type: 'href',
+      value: '/docs/intro',
+    },
   },
 ];
 
@@ -73,15 +128,13 @@ test.describe('тесты страницы playwright', () => {
   });
 
   test('Проверка href', async ({ page }) => {
-    await expect(page.getByRole('link', { name: 'Docs' })).toHaveAttribute('href', '/docs/intro');
-    await expect(page.getByRole('link', { name: 'MCP', exact: true })).toHaveAttribute(
-      'href',
-      '/mcp/introduction',
-    );
-    await expect(page.getByRole('link', { name: 'CLI', exact: true })).toHaveAttribute(
-      'href',
-      '/agent-cli/introduction',
-    );
+    elements.forEach(({ locator, name, attribute }) => {
+      if (attribute) {
+        test.step(`Проверка аттрибута href у элемента ${name}`, async () => {
+          await expect.soft(locator(page)).toHaveAttribute(attribute?.type, attribute?.value);
+        });
+      }
+    });
   });
 
   test('Проверка dark-mode', async ({ page }) => {
@@ -89,17 +142,17 @@ test.describe('тесты страницы playwright', () => {
     await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
   });
 
-  test('Проверка заголовка', async ({ page }) => {
-    await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toContainText(
-      'Playwright enables reliable web automation for testing, scripting, and AI agents.',
-    );
-  });
+  // test('Проверка заголовка', async ({ page }) => {
+  //   await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toBeVisible();
+  //   await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toContainText(
+  //     'Playwright enables reliable web automation for testing, scripting, and AI agents.',
+  //   );
+  // });
 
-  test('Проверка кнопки Get Started', async ({ page }) => {
-    await expect.soft(page.getByRole('link', { name: 'Get started' })).toBeVisible();
-    await expect
-      .soft(page.getByRole('link', { name: 'Get started' }))
-      .toHaveAttribute('href', '/docs/intro');
-  });
+  // test('Проверка кнопки Get Started', async ({ page }) => {
+  //   await expect.soft(page.getByRole('link', { name: 'Get started' })).toBeVisible();
+  //   await expect
+  //     .soft(page.getByRole('link', { name: 'Get started' }))
+  //     .toHaveAttribute('href', '/docs/intro');
+  // });
 });
