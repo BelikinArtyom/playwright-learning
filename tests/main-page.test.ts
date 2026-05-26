c6af838b48779f87c5a53fc1216a0da9789743e8
Artyom Belikin <biloplus@yandex.ru>
2026-05-26 16:06:02 +0300

    new-commit

--- tests/main-page.test.ts
+++ tests/main-page.test.ts
@@ -1,33 +1,75 @@
 import { test, expect } from '@playwright/test';
 
+const elements = [
+  {
+    locator: (page) => page.getByRole('link', { name: 'Playwright logo Playwright' }),
+    name: 'Playwright logo',
+    text: 'Playwright',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'Docs' }),
+    name: 'Docs link',
+    text: 'Docs',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'MCP', exact: true }),
+    name: 'MCP link',
+    text: 'MCP',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'CLI', exact: true }),
+    name: 'CLI link',
+    text: 'CLI',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'API', exact: true }),
+    name: 'API link',
+    text: 'API',
+  },
+  {
+    locator: (page) => page.getByRole('button', { name: 'Node.js' }),
+    name: 'Node.js link',
+    text: 'Node.js',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'GitHub repository' }),
+    name: 'Github icon link',
+  },
+  {
+    locator: (page) => page.getByRole('link', { name: 'Discord server' }),
+    name: 'Driscord icon link',
+  },
+  {
+    locator: (page) => page.getByRole('button', { name: 'Switch between dark and light' }),
+    name: 'Dark mode switch',
+  },
+  {
+    locator: (page) => page.getByRole('button', { name: 'Search (Ctrl+K)' }),
+    name: 'Search input',
+  },
+];
+
 test.describe('тесты страницы playwright', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://playwright.dev/');
   });
 
-  test('Проверка отображения элементов навигации', async ({ page }) => {
-    await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
-    await page.getByRole('link', { name: 'Docs' }).click();
-    await expect(page.getByRole('link', { name: 'MCP', exact: true })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'CLI', exact: true })).toBeVisible();
-    await page.getByRole('link', { name: 'API', exact: true }).click();
-    await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Discord server' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Switch between dark and light' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Search (Ctrl+K)' })).toBeVisible();
+  test('Проверка отображения элементов навигации хедера', async ({ page }) => {
+    elements.forEach(({ locator, name }) => {
+      test.step(`Проверка отображения элемента ${name}`, async () => {
+        await expect.soft(locator(page)).toBeVisible();
+      });
+    });
   });
 
   test('Проверка названий элементов хедера', async ({ page }) => {
-    await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
-      'Playwright',
-    );
-    await expect(page.getByRole('link', { name: 'Docs' })).toContainText('Docs');
-    await expect(page.getByRole('link', { name: 'MCP', exact: true })).toContainText('MCP');
-    await expect(page.getByRole('link', { name: 'CLI', exact: true })).toContainText('CLI');
-    await expect(page.getByRole('link', { name: 'API' })).toContainText('API');
-    await expect(page.getByRole('button', { name: 'Node.js' })).toContainText('Node.js');
-    await expect(page.getByRole('button', { name: 'Search (Ctrl+K)' })).toContainText('Search');
+    elements.forEach(({ locator, name, text }) => {
+      if (text) {
+        test.step(`Проверка отображения названия элемента ${name}`, async () => {
+          await expect.soft(locator(page)).toContainText(text);
+        });
+      }
+    });
   });
 
   test('Проверка href', async ({ page }) => {
